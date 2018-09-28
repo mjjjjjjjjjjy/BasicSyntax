e0b54246aed1e4ebb3f418b10d8201330f5868db
MoJianyue <1194229162@qq.com>
2018-09-28 10:38:04 +0800

    22

--- src/main/java/basic/BasicSyntaxApplication.java
+++ src/main/java/basic/BasicSyntaxApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BasicSyntaxApplication {
 
 	public static void main(String[] args) {
+	    
 		SpringApplication.run(BasicSyntaxApplication.class, args);
 	}
 	
